aa999e1b51d5e0768aeafaa984de78820b282fdc
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2022-10-29 01:05:34 +0400

    Action prototype wrote

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# install-dnscontrol
+# Install [DNSControl][dnscontrol] action
+
+![Release version][badge_release_version]
+[![Build Status][badge_build]][link_build]
+[![License][badge_license]][link_license]
+
+This action installs [DNSControl][dnscontrol] as a binary file into your workflow. It can be run on **Linux** (`ubuntu-latest`) or **macOS** (`macos-latest`).
+
+- 🚀 DNSControl releases page: <https://github.com/StackExchange/dnscontrol/releases>
+
+Additionally, this action uses GitHub **caching mechanism** to speed up your workflow execution time!
+
+## Usage
+
+```yaml
+jobs:
+  install-dnscontrol:
+    runs-on: ubuntu-20.04
+    steps:
+      - uses: gacts/install-dnscontrol@v1
+        #with:
+        #  version: 1.2.0 # `latest` by default, but you can set a specific version to install, e.g.: `3.20.0`
+
+      - run: dnscontrol version # any dnscontrol command can be executed
+```
+
+## Customizing
+
+### Inputs
+
+Following inputs can be used as `step.with` keys:
+
+| Name           |   Type   |        Default        | Required | Description                                                      |
+|----------------|:--------:|:---------------------:|:--------:|------------------------------------------------------------------|
+| `version`      | `string` |       `latest`        |    no    | DNSControl version to install                                    |
+| `github-token` | `string` | `${{ github.token }}` |    no    | GitHub token (for requesting the latest DNSControl version info) |
+
+### Outputs
+
+| Name               |   Type   | Description                          |
+|--------------------|:--------:|--------------------------------------|
+| `  dnscontrol-bin` | `string` | Path to the   dnscontrol binary file |
+
+## Releasing
+
+New versions releasing scenario:
+
+- Make required changes in the [changelog](CHANGELOG.md) file
+- Build the action distribution (`make build` or `yarn build`)
+- Commit and push changes (including `dist` directory changes - this is important) into the `master` branch
+- Publish new release using repo releases page (git tag should follow `vX.Y.Z` format)
+
+Major and minor git tags (`v1` and `v1.2` if you publish `v1.2.Z` release) will be updated automatically.
+
+## Support
+
+[![Issues][badge_issues]][link_issues]
+[![Issues][badge_pulls]][link_pulls]
+
+If you find any action errors, please, [make an issue][link_create_issue] in the current repository.
+
+## License
+
+This is open-sourced software licensed under the [MIT License][link_license].
+
+[badge_build]:https://img.shields.io/github/workflow/status/gacts/install-dnscontrol/tests?maxAge=30
+[badge_release_version]:https://img.shields.io/github/release/gacts/install-dnscontrol.svg?maxAge=30
+[badge_license]:https://img.shields.io/github/license/gacts/install-dnscontrol.svg?longCache=true
+[badge_release_date]:https://img.shields.io/github/release-date/gacts/install-dnscontrol.svg?maxAge=180
+[badge_commits_since_release]:https://img.shields.io/github/commits-since/gacts/install-dnscontrol/latest.svg?maxAge=45
+[badge_issues]:https://img.shields.io/github/issues/gacts/install-dnscontrol.svg?maxAge=45
+[badge_pulls]:https://img.shields.io/github/issues-pr/gacts/install-dnscontrol.svg?maxAge=45
+
+[link_build]:https://github.com/gacts/install-dnscontrol/actions
+[link_license]:https://github.com/gacts/install-dnscontrol/blob/master/LICENSE
+[link_issues]:https://github.com/gacts/install-dnscontrol/issues
+[link_create_issue]:https://github.com/gacts/install-dnscontrol/issues/new
+[link_pulls]:https://github.com/gacts/install-dnscontrol/pulls
+
+[dnscontrol]:https://github.com/StackExchange/dnscontrol
---
+++ src/index.js
@@ -0,0 +1,153 @@
+const core = require('@actions/core') // docs: https://github.com/actions/toolkit/tree/main/packages/core
+const tc = require('@actions/tool-cache') // docs: https://github.com/actions/toolkit/tree/main/packages/tool-cache
+const github = require('@actions/github') // docs: https://github.com/actions/toolkit/tree/main/packages/github
+const io = require('@actions/io') // docs: https://github.com/actions/toolkit/tree/main/packages/io
+const cache = require('@actions/cache') // docs: https://github.com/actions/toolkit/tree/main/packages/cache
+const exec = require('@actions/exec') // docs: https://github.com/actions/toolkit/tree/main/packages/exec
+const path = require('path')
+const os = require('os')
+
+// read action inputs
+const input = {
+  version: core.getInput('version', {required: true}).replace(/^v/, ''), // strip the 'v' prefix
+  githubToken: core.getInput('github-token'),
+}
+
+// main action entrypoint
+async function runAction() {
+  let version
+
+  if (input.version.toLowerCase() === 'latest') {
+    core.debug('Requesting latest DNSControl version...')
+    version = await getLatestDNSControlVersion(input.githubToken)
+  } else {
+    version = input.version
+  }
+
+  core.startGroup('💾 Install DNSControl')
+  await doInstall(version)
+  core.endGroup()
+
+  core.startGroup('🧪 Installation check')
+  await doCheck()
+  core.endGroup()
+}
+
+/**
+ * @param {string} version
+ *
+ * @returns {Promise<void>}
+ *
+ * @throws
+ */
+async function doInstall(version) {
+  const pathToInstall = path.join(os.tmpdir(), `dnscontrol-${version}`)
+  const cacheKey = `dnscontrol-cache-${version}-${process.platform}-${process.arch}`
+
+  core.info(`Version to install: ${version} (target directory: ${pathToInstall})`)
+
+  let restoredFromCache = undefined
+
+  try {
+    restoredFromCache = await cache.restoreCache([pathToInstall], cacheKey)
+  } catch (e) {
+    core.warning(e)
+  }
+
+  if (restoredFromCache !== undefined) { // cache HIT
+    core.info(`👌 DNSControl restored from cache`)
+  } else { // cache MISS
+    const distUri = getDNSControlURI(process.platform, process.arch, version)
+    const distPath = await tc.downloadTool(distUri, path.join(os.tmpdir(), `dnscontrol.tmp`))
+    const binPath = path.join(pathToInstall, 'dnscontrol')
+
+    await io.mkdirP(pathToInstall)
+    await io.mv(distPath, binPath)
+    await fs.chmod(binPath, 0o755)
+
+    try {
+      await cache.saveCache([pathToInstall], cacheKey)
+    } catch (e) {
+      core.warning(e)
+    }
+  }
+
+  core.addPath(pathToInstall)
+}
+
+/**
+ * @returns {Promise<void>}
+ *
+ * @throws
+ */
+async function doCheck() {
+  const dnscontrolBinPath = await io.which('dnscontrol', true)
+
+  if (dnscontrolBinPath === "") {
+    throw new Error('dnscontrol binary file not found in $PATH')
+  }
+
+  await exec.exec('dnscontrol', ['version'], {silent: true})
+
+  core.setOutput('dnscontrol-bin', dnscontrolBinPath)
+
+  core.info(`DNSControl installed: ${dnscontrolBinPath}`)
+}
+
+/**
+ * @param {string} githubAuthToken
+ * @returns {Promise<string>}
+ */
+async function getLatestDNSControlVersion(githubAuthToken) {
+  const octokit = github.getOctokit(githubAuthToken)
+
+  // docs: https://octokit.github.io/rest.js/v18#repos-get-latest-release
+  const latest = await octokit.rest.repos.getLatestRelease({
+    owner: 'StackExchange',
+    repo: 'dnscontrol',
+  })
+
+  return latest.data.tag_name.replace(/^v/, '') // strip the 'v' prefix
+}
+
+/**
+ * @link https://github.com/StackExchange/dnscontrol/releases
+ *
+ * @param {('linux'|'darwin'|'win32')} platform
+ * @param {('x32'|'x64'|'arm'|'arm64')} arch
+ * @param {string} version E.g.: `1.2.6`
+ *
+ * @returns {string}
+ *
+ * @throws
+ */
+function getDNSControlURI(platform, arch, version) {
+  switch (platform) {
+    case 'linux': {
+      switch (arch) {
+        case 'x64': // Amd64
+          return `https://github.com/StackExchange/dnscontrol/releases/download/v${version}/dnscontrol-Linux`
+      }
+
+      throw new Error('Unsupported linux architecture')
+    }
+
+    case 'darwin': {
+      switch (arch) {
+        case 'arm64':
+          return `https://github.com/StackExchange/dnscontrol/releases/download/v${version}/dnscontrol-Darwin`
+      }
+
+      throw new Error('Unsupported MacOS architecture')
+    }
+  }
+
+  throw new Error('Unsupported OS (platform)')
+}
+
+// run the action
+(async () => {
+  await runAction()
+})().catch(error => {
+  core.setFailed(error.message)
+})
