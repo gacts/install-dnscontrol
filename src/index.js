bd76d0934fea4543e839d00d9104c5f6fefc495b
Pаramtamtām <7326800+tarampampam@users.noreply.github.com>
2025-11-03 19:30:33 +0400

    Upgrade Node to v24 and dependencies (#79)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const core = require('@actions/core') // docs: https://github.com/actions/toolkit/tree/main/packages/core
-const tc = require('@actions/tool-cache') // docs: https://github.com/actions/toolkit/tree/main/packages/tool-cache
-const io = require('@actions/io') // docs: https://github.com/actions/toolkit/tree/main/packages/io
-const cache = require('@actions/cache') // docs: https://github.com/actions/toolkit/tree/main/packages/cache
-const exec = require('@actions/exec') // docs: https://github.com/actions/toolkit/tree/main/packages/exec
-const path = require('path')
-const os = require('os')
-const http = require('@actions/http-client') // https://github.com/actions/toolkit/tree/main/packages/http-client
+import core from '@actions/core' // docs: https://github.com/actions/toolkit/tree/main/packages/core
+import tc from '@actions/tool-cache' // docs: https://github.com/actions/toolkit/tree/main/packages/tool-cache
+import io from '@actions/io' // docs: https://github.com/actions/toolkit/tree/main/packages/io
+import cache from '@actions/cache' // docs: https://github.com/actions/toolkit/tree/main/packages/cache
+import exec from '@actions/exec' // docs: https://github.com/actions/toolkit/tree/main/packages/exec
+import path from 'path'
+import os from 'os'
+import http from '@actions/http-client' // https://github.com/actions/toolkit/tree/main/packages/http-client
 
 // read action inputs
 const input = {
